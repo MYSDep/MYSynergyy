e29df5411213c2feb62f82481a2959a6040ea445
lupin <lupin@lupin.com>
2026-03-25 14:42:44 +0200

    add

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, "index.html");
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Error loading index.html");
+      return;
+    }
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
